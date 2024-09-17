1b2a624cd96c09b05b955841ad51a6183402cb10
isum hettiarachchi <isumanmitha@gmail.com>
2024-09-17 20:51:11 +0530

    changes in index.php

--- index.php
+++ index.php
@@ -35,6 +35,24 @@
         </div>
     
     </header>
+    <div class="top-background">
+        <h1>IT'S PARTY TIME!!<br>GET GROOVY WITH GROOVY.</h1>
+        <div class="bookbtn">
+            <a href="#">Book now</a>
+            <!-- <script type="text/javascript">
+                if($_SESS)
+            </script> -->
+        </div>
+    </div>
+    <div class="packages-container">
+        <div class="packages-list">
+            <ul>
+                <li><div class="card" id="one"><h2>Birthday's</h2></div></li>
+                <li><div class="card" id="two"><h2>Batch Parties</h2></div></li>
+                <li><div class="card" id="three"><h2>Graguations</h2></div></li>
+            </ul>
+        </div>
+    </div>
     <h1>Welcome, 
         !</h1>
     <p>This is your dashboard.</p>
